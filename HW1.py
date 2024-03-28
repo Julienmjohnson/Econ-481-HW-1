9e20ae7b66d69e2a84fbb4a4712f5683ea7a651b
Julien <julienmj@uw.edu>
2024-03-28 20:10:36 +0000

    updated HW1. should have exercise 1

--- HW1.py
+++ HW1.py
@@ -1,3 +1,4 @@
+#Exercise 0
 def github() -> str:
     """
     Some docstrings.
@@ -5,5 +6,20 @@ def github() -> str:
 
     return "https://github.com/Julienmjohnson/Econ-481-HW-1/blob/main/HW1.py"
 
-print("Wow!")
-print("OMG!")
+#Exercise 2
+def evens_and_odds(x: int) -> dict:
+    """
+    this function takes a natural number n as an input and returns a dictionary where the key 'evens' is the sum of all even terms less than n and where the key 'odds' is the sum of all terms less than n
+    """
+    evens = 0
+    odds = 0
+    i = 0
+    while (i < x):
+        evens += i
+        i += 2
+    i = 1
+    while (i < x):
+        odds += i
+        i += 2
+        
+    return {'evens':evens, 'odds':odds}
